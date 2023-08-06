edb94a93f9b62260a76f37746d1a6f1fccb5173d
Pasumarthi Kumar <aswin.important@yahoo.com>
2023-08-06 08:50:08 +0530

    Hello World Spring Boot application.

---
+++ src/test/java/com/learn/greet/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.greet.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
